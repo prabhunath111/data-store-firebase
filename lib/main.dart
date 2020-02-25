9805bb5a49d887e5d0725e01bd50fc4b86777441
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-02-25 18:45:55 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              document['name'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(color: Color(0xffddddff)),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              document['votes'].toString(),
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ],
+      ),
+      onTap: () {
+        document.reference.updateData({'votes': document['votes'] + 1});
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data store'),
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection('bandnames').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return ListView.builder(
+              itemExtent: 80.0,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          }),
+    );
+  }
+}
